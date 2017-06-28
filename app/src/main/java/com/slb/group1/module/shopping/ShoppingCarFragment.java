fc3b8c062774dde3028846776285ff6d05971b47
wangyikai1234567 <2285281993@qq.com>
2017-06-28 11:02:00 +0800

    添加了一个购物车fragment

---
+++ app/src/main/java/com/slb/group1/module/shopping/ShoppingCarFragment.java
@@ -0,0 +1,31 @@
+package com.slb.group1.module.shopping;
+
+import com.slb.group1.module.base.BaseFragment;
+
+/**
+ * date: 2017/6/28.
+ * author: 王艺凯 (lenovo )
+ * function:
+ */
+
+public class ShoppingCarFragment extends BaseFragment{
+    @Override
+    protected int attachLayoutRes() {
+        return 0;
+    }
+
+    @Override
+    protected void initInjector() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void updateViews(boolean isRefresh) {
+
+    }
+}
